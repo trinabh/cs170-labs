bfee1c14c595ee355b52a78af7e70ffb2a52c04a
Jason Wolfe <jason23@cs.utexas.edu>
2012-01-18 20:25:38 -0600

    This is the base merge.c. Resolve the conflict so that the merged file will print the same lines.

---
+++ merge.c
@@ -0,0 +1,17 @@
+//
+// Simple program used to demonstrate a git-merge
+// 
+// Base version.
+//
+// For JOS lab 1
+//
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+        printf("A\n");
+        printf("2\n");
+        printf("C\n");
+}
